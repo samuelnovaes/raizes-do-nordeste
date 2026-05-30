945c480c096d8fc05bf42ce18693b0eb6c9d0301
Samuel Sena <samuel.sena@litroz.com>
2026-05-30 17:06:13 -0300

    Adiciona configuracao de TypeScript, ESLint e cobertura de testes

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unsafe-declaration-merging': 'off'
+    }
+  }
+];
